dc741dec1835cd770fd135ba5abfac6caa76515a
nrgould <nrgould197@gmail.com>
2024-08-14 14:26:35 -0400

    refactored themed text to use Shopify Restyle

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,18 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
+import React from 'react';
+import { TextProps as RNTextProps } from 'react-native';
+import { createText, useTheme } from '@shopify/restyle';
+import { Theme } from '@/constants/theme';
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+const Text = createText<Theme>();
 
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+export type ThemedTextProps = RNTextProps & {
+	type?: 'header' | 'subheader' | 'body' | 'caption' | 'button';
 };
 
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+export function ThemedText({ type = 'body', style, ...rest }: ThemedTextProps) {
+	const theme = useTheme<Theme>();
+	const color =
+		theme.colors[type === 'button' ? 'buttonText' : 'textPrimary'];
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+	return <Text variant={type} style={[{ color }, style]} {...rest} />;
 }
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
