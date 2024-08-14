0c7272db8bdf7a719b05b28a22d0d234595e424d
nrgould <nrgould197@gmail.com>
2024-08-14 14:45:13 -0400

    EXPO BROKEN

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,7 @@
-import { Image, StyleSheet, TextInput } from 'react-native';
-import ParallaxScrollView from '@/components/organisms/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
+import { ThemedText } from '@/components/atoms/ThemedText';
 import Box from '../../components/atoms/Box';
 import { useEffect, useState } from 'react';
-import Button from '@/components/atoms/Button';
 import OpenAI from 'openai';
-import ThemedButton from '@/components/molecules/ThemedButton';
-import ThemedTextInput from '@/components/atoms/ThemedTextInput';
 import ChatForm from '@/components/molecules/ChatForm';
 
 export default function HomeScreen() {
@@ -67,22 +62,3 @@ export default function HomeScreen() {
 		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	titleContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 8,
-	},
-	stepContainer: {
-		gap: 8,
-		marginBottom: 8,
-	},
-	reactLogo: {
-		height: '100%',
-		width: '100%',
-		bottom: 0,
-		left: 0,
-		position: 'absolute',
-	},
-});
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,34 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/atoms/ThemedText';
+import Box from '@/components/atoms/Box';
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+	return (
+		<>
+			<Stack.Screen options={{ title: 'Oops!' }} />
+			<Box style={styles.container}>
+				<ThemedText type='header'>
+					This screen doesn't exist.
+				</ThemedText>
+				<Link href='/' style={styles.link}>
+					<ThemedText type='body'>Go to home screen!</ThemedText>
+				</Link>
+			</Box>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 20,
+	},
+	link: {
+		marginTop: 15,
+		paddingVertical: 15,
+	},
 });
